a7cb3962076cda2eb49e77bd149c1b04873df9ea
BryanAlania <1359537@senati.pe>
2022-11-14 06:49:33 -0500

    aplique una plantilla para ver como se veria

---
+++ resources/views/index.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<!--
+	The7 by TEMPLATE STOCK
+	templatestock.co @templatestock
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title>huayllay</title>
+        
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/simplegrid.css">
+        <link rel="stylesheet" href="css/icomoon.css">
+        <link rel="stylesheet" href="css/lightcase.css">
+        <link rel="stylesheet" href="js/owl-carousel/owl.carousel.css" />
+        <link rel="stylesheet" href="js/owl-carousel/owl.theme.css" />
+        <link rel="stylesheet" href="js/owl-carousel/owl.transitions.css" />
+        <link rel="stylesheet" href="style.css">
+
+        <!-- Google Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,900' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+           <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+           <!--[if lt IE 9]>
+             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+           <![endif]-->
+        </head>
+        <body id="home">
+            <!-- Header -->
+            <header id="top-header" class="header-home">
+                <div class="grid">
+                    <div class="col-1-1">
+                        <div class="content">
+                            <div class="logo-wrap">
+                                <a href="#0" class="logo"></a>
+                            </div>
+                            <nav class="navigation">
+                                <input type="checkbox" id="nav-button">
+                                <label for="nav-button" onclick></label>
+                                <ul class="nav-container">
+                                    <li><a href="#home" class="current">Home</a></li>
+                                    <li><a href="#services">Services</a></li>
+                                    <li><a href="#work">Work</a></li>
+                                    <li><a href="#blog">Blog</a></li>
+                                    <li><a href="#pricing">Pricing</a></li>
+                                    <li><a href="#team">Team</a></li>
+                                    <li><a href="#contact">Contact</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!-- End Header -->
+
+            <!-- Parallax Section -->
+            <div class="parallax-section parallax1">
+                <div class="grid grid-pad">
+                    <div class="col-1-1">
+                         <div class="content content-header" >
+                            <h2>We Are Creative Design Agency</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, fuga, consectetur sequi consequuntur nisi placeat ullam maiores perferendis. Quod, nihil reiciendis saepe optio libero minus et beatae ipsam reprehenderit sequi.</p>
+                            <a target="_blank" class="btn btn-ghost" href="#">Find Out More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Parallax Section -->
+
+            <!-- CurveUp -->
+            <svg class="curveUpColor" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none">
+                <path d="M0 100 C 20 0 50 0 100 100 Z"></path>
+            </svg>
+
+            <!-- Services Section -->
+            <div class="wrap services-wrap" id="services">
+                <section class="grid grid-pad services">
+                    <h2>Our Services</h2>
+                    <div class="col-1-4 service-box service-1" >
+                        <div class="content">
+                            <div class="service-icon">
+                                <i class="circle-icon icon-heart4"></i>
+                            </div>
+                            <div class="service-entry">
+                                <h3>Lovely Design</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat tellus eget libero pretium, sollicitudin feugiat libero.</p>
+                                <a class="btn read-more" href="#0">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-1-4 service-box service-2" >
+                        <div class="content">
+                            <div class="service-icon">
+                                <i class="circle-icon icon-star4"></i>
+                            </div>
+                            <div class="service-entry">
+                                <h3>Great Concept</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat tellus eget libero pretium, sollicitudin feugiat libero.</p>
+                                <a class="btn read-more" href="#0">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-1-4 service-box service-3">
+                        <div class="content">
+                            <div class="service-icon">
+                                <i class="circle-icon icon-display"></i>
+                            </div>
+                            <div class="service-entry">
+                                <h3>Development</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat tellus eget libero pretium, sollicitudin feugiat libero.</p>
+                                <a class="btn read-more" href="#0">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-1-4 service-box service-4" >
+                        <div class="content">
+                            <div class="service-icon">
+                                <i class="circle-icon icon-user6"></i>
+                            </div>
+                            <div class="service-entry">
+                                <h3>User Friendly</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat tellus eget libero pretium, sollicitudin feugiat libero.</p>
+                                <a class="btn read-more" href="#0">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            
+        </body>
+        </html>
